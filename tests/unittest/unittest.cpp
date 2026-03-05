aad4f3cbb8cddd4f1046430b2738bdd4ef016b0f
My Name <217945237+nevergiveupcpp@users.noreply.github.com>
2026-03-05 20:06:46 +0700

    chore: add find_package support, redesign project structure, separate tests

--- tests/unittest/unittest.cpp
+++ tests/unittest/unittest.cpp
@@ -15,8 +15,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef NGU_OBFUSCXX_METRICS_UNIT_TEST_H
-#define NGU_OBFUSCXX_METRICS_UNIT_TEST_H
+#ifndef NGU_OBFUSCXX_METRICS_UNITTEST_H
+#define NGU_OBFUSCXX_METRICS_UNITTEST_H
 
 #include <array>
 
